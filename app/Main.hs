68ce3c7f8be7d0e95185529217c79e7b831b6064
François Malabre <francois@mewo.io>
2021-03-09 23:49:30 +0100

    refactor

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,53 @@
-{-# LANGUAGE ExtendedDefaultRules #-}
-{-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE NoImplicitPrelude    #-}
 {-# LANGUAGE OverloadedStrings    #-}
-{-# LANGUAGE ScopedTypeVariables  #-}
 
 module Main where
 
-import           Lib
+import           HBB
 import           Relude
 import           System.Environment (getArgs, getProgName, setEnv, unsetEnv)
 
+
+type ProgName = String
+type Args = [String]
+
+helpMessage :: Text
+helpMessage = "hbb-exe false|true|yes|..."
+
+printHelp :: IO ()
+printHelp = putTextLn helpMessage
+
+printError :: ProgName -> Args -> IO ()
+printError progName args = do
+  putTextLn $ unlines
+    [ unwords ["progName", toText progName]
+    , unwords $ "args" : (toText <$> args)
+    ]
+  exitFailure
+
+
+mainWithArgs :: ProgName -> Args -> IO ()
+mainWithArgs progName args = case (progName, args) of
+  ("cat"    , _                ) -> forM_ args cat
+  ("echo"   , "-n" : args'     ) -> putText $ argsToText args'
+  ("echo"   , _                ) -> putTextLn $ argsToText args
+  ("false"  , _                ) -> exitFailure
+  ("sh"     , []               ) -> sh
+  ("true"   , _                ) -> exitSuccess
+  ("wc"     , ["-c", file]     ) -> wcC file
+  ("wc"     , ["-l", file]     ) -> wcL file
+  ("yes"    , []               ) -> yes "y"
+  ("yes"    , _                ) -> yes $ argsToText args
+  ("hbb-exe", ["-h"]           ) -> printHelp
+  ("hbb-exe", ["--help"]       ) -> printHelp
+  ("hbb-exe", progName' : args') -> mainWithArgs progName' args'
+  _                              -> printHelp >> printError progName args
+  where
+    argsToText :: Args -> Text
+    argsToText = unwords . fmap toText
+
 main :: IO ()
 main = do
-  progName <- fmap toText getProgName
-  args     <- fmap (fmap toText) getArgs
+  progName <- getProgName
+  args <- getArgs
   mainWithArgs progName args
---
+++ src/HBB.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE NoImplicitPrelude    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+
+module HBB where
+
+import qualified Control.Exception.Safe as CES
+import           Control.Monad          ((>=>))
+import qualified Data.Text              as T
+import qualified Data.Text.IO           as TIO
+import           Relude
+import qualified System.Directory       as SD
+import           System.Environment     (getArgs, getProgName, setEnv, unsetEnv)
+import qualified System.Exit            as SE
+import qualified System.IO              as SIO
+import qualified System.IO.Error        as SIOE
+import qualified System.Process         as SP
+
+
+yes :: Text -> IO ()
+yes = forever . putTextLn
+
+readingFile :: (Text -> IO a) -> FilePath -> IO a
+readingFile fun file = withFile file ReadMode $ TIO.hGetContents >=> fun
+
+wcC :: FilePath -> IO ()
+wcC = readingFile $ putTextLn . show . T.length
+
+wcL :: FilePath -> IO ()
+wcL = readingFile $ putTextLn . show . length . lines
+
+cat :: FilePath -> IO ()
+cat = readingFile putText
+
+sh = do
+  SIO.hSetBuffering SIO.stderr SIO.NoBuffering
+  SIO.hSetBuffering SIO.stdin SIO.NoBuffering
+  SIO.hSetBuffering SIO.stdout SIO.NoBuffering
+  putText "> "
+  line   <- getLine
+  result <- CES.tryAny $ case words line of
+    ["exit"] -> exitSuccess
+    ["cd"  ] -> SD.getHomeDirectory >>= SD.setCurrentDirectory
+    ["cd"   , d              ] -> SD.setCurrentDirectory $ toString d
+    ["unset", name           ] -> unsetEnv $ toString name
+    ["set"  , nameEqualsValue] -> case T.split ('=' ==) nameEqualsValue of
+      [name , value] -> setEnv (toString name) (toString value)
+      _              -> exitFailure
+    (progName : args) -> do
+      let process = SP.proc (toString progName) (toString <$> args)
+      eitherExceptionOrExitCode <-
+        SP.withCreateProcess process { SP.delegate_ctlc = True }
+          $ \_ _ _ p -> SP.waitForProcess p
+      case eitherExceptionOrExitCode of
+        SE.ExitSuccess   -> pass
+        SE.ExitFailure r -> putText $ show r
+    [] -> pass
+
+  case result of
+    Right () -> pass
+    Left  e  -> case fromException e of
+      Just SE.ExitSuccess -> exitSuccess
+      _                   -> do
+        putStrLn $ displayException e
+        putStr "err"
+  sh
